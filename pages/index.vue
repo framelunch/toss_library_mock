1d64fa5f808b4da67ebad99f7655659fa6509f2a
picnium14 <i.sato@framelunch.co.jp>
2021-04-15 21:04:55 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        {{ text }}
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "@vue/composition-api"
+
+export default defineComponent({
+  setup (_) {
+    /* Reactive State */
+    const reactiveState = reactive({
+      text: 'hello!'
+    })
+
+    /* Methods */
+    const methods = {
+    }
+
+    return {
+      ...toRefs(reactiveState),
+      ...methods
+    }
+  }
+})
+</script>
