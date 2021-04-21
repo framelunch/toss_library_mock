800dbec7e2d26a6778b0b0a9af3e2f78e2f5f810
picnium14 <i.sato@framelunch.co.jp>
2021-04-21 15:09:03 +0900

    search loadingを変えてみる

--- components/parts/Loading.vue
+++ components/parts/Loading.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container style="height: 100vh;">
     <div class="loading">
-      <div class="mainspin">
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 19c-3.87 0-7-3.13-7-7 0-3.53 2.61-6.43 6-6.92V2.05c-5.06.5-9 4.76-9 9.95 0 5.52 4.47 10 9.99 10 3.31 0 6.24-1.61 8.06-4.09l-2.6-1.53C16.17 17.98 14.21 19 12 19z" />
-        </svg>
+      <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
       </div>
     </div>
   </v-container>
@@ -42,15 +42,26 @@ export default defineComponent({
   }
 }
 
-.mainspin {
-  animation: spin_frames 2s linear 0s infinite;
-  fill: #e4e4e4;
-	height: 10vw;
-	width: 10vw;
+.spinner {
+  text-align: center;
+  width: 70px;
+}
 
-  @media (--sm) {
-    height: 20vw;
-    width: 20vw;
-  }
+.spinner > div {
+  animation: sk-bouncedelay 1.2s infinite ease-in-out both;
+  background-color: #e4e4e4;
+  border-radius: 100%;
+  display: inline-block;
+  margin: 0 4px;
+  height: 12px;
+  width: 12px;
+}
+
+.spinner .bounce1 {
+  animation-delay: -0.32s;
+}
+
+.spinner .bounce2 {
+  animation-delay: -0.16s;
 }
 </style>
