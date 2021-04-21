f9c47b5da75bfb0b665913bfd57a875ad0737a19
picnium14 <i.sato@framelunch.co.jp>
2021-04-21 17:59:12 +0900

    修正

--- components/pages/top/parts/TeacherListSp.vue
+++ components/pages/top/parts/TeacherListSp.vue
@@ -92,7 +92,20 @@ export default defineComponent({
     /* Methods */
     const methods = {
       toSearch (teacher: any) {
-        context.root.$router.push({path: 'search', query: { teacher: `${ teacher.value }` } })
+        const slider = document.querySelectorAll('.slider')
+        slider.forEach((item) => {
+          item.classList.add('-hidden')
+        })
+
+        setTimeout(() => {
+          context.root.$router.push({path: 'search', query: { teacher: `${ teacher.value }` } })
+        }, 110)
+
+        setTimeout(() => {
+          slider.forEach((item) => {
+            item.classList.remove('-hidden')
+          })
+        }, 1000)
       }
     }
 
